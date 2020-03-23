2630f720ad823c28cbc8be3b087d394143bc6dfa
Christine Mathews <christinemathews@Christines-MacBook-Air.local>
2020-03-24 00:25:52 +0530

    first commit

---
+++ webview/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'pages/webview.dart';
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner:false,
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+      title: new Text(
+        'Welcome In SplashScreen',
+        style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+      ),
+      seconds: 8,
+      navigateAfterSeconds: WebViews(),
+      image: new Image.asset(
+          'assets/gif/loading.gif'),
+      backgroundColor: Colors.black,
+      styleTextUnderTheLoader: new TextStyle(),
+      photoSize: 150.0,
+      onClick: () => print("Flutter Egypt"),
+      loaderColor: Colors.white,
+    );
+  }
+}
